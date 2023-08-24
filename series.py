58df5a142328ff0909d3795383daa4232059bc78
Rutujakodag1 <141642777+Rutujakodag1@users.noreply.github.com>
2023-08-24 22:16:54 +0530

    series.py
    Program to fibonacci series

---
+++ series.py
@@ -0,0 +1,12 @@
+//Fibonacci series:
+n=int(input("Enter size of series"))
+first=0
+second=1
+print(first)
+print(second)
+for i in range(n-2):
+  next=first+second
+  print(next)
+  temp=first
+  first=second
+  second=next
