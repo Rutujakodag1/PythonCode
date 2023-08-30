627e5db91eeae214de67ccefff6eae8b58c01092
Rutujakodag1 <141642777+Rutujakodag1@users.noreply.github.com>
2023-08-30 21:13:23 +0530

    SI.py
    program to calculate Simple interest

---
+++ SI.py
@@ -0,0 +1,5 @@
+#Calculate simple interest:
+p=int(input("Enter the principle:"))
+rate=int(input("Enter the rate of interest:"))
+time=float(input("Enter the period :"))
+print("Simple interest:",(p*rate*time)/100)
