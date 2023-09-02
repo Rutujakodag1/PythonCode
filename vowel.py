a247be82dd047cce27633feeb863526003ea5faf
Rutujakodag1 <141642777+Rutujakodag1@users.noreply.github.com>
2023-09-02 22:00:36 +0530

    vowel.py
    to check given character is vowel or consonant

--- vowel.py
+++ vowel.py
@@ -1,6 +1,7 @@
 #Check given character is vowel or consonant
 ch=input("Enter the character to check :")
-if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U':
+str1='aeiouAEIOU'
+if ch in str1:
   print("vowel")
 else:
   print("consonant")
