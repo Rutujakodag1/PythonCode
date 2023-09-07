4b69e450bb2a82d9b12727dd1777df269bf1491b
Rutujakodag1 <141642777+Rutujakodag1@users.noreply.github.com>
2023-09-07 18:32:22 +0530

    Anagramornot.py

--- Anagramornot.py
+++ Anagramornot.py
@@ -1,4 +1,4 @@
-//Anagram or not
+#Anagram or not
 str1=input("Enter the string:")
 str2=input("Enter the string:")
 l3=0
@@ -9,12 +9,9 @@ if len1==len2:
   for i in str1:
     if i in str2:
       l3=l3+1
-
   for i in str2:
     if i in str1:
       l4=l4+1
-
-
 if l3==len1 and l4==len2:
   print("It is anagram")
 else:
