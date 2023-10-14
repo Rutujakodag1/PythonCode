e431d3c87de0493b0034c75ff33d30371e3cbd46
Rutujakodag1 <141642777+Rutujakodag1@users.noreply.github.com>
2023-10-14 22:14:22 +0530

    replacespace.py
    program for replace the space by given character

---
+++ replacespace.py
@@ -0,0 +1,5 @@
+#program to replace space by given character:
+str=input("Enter the string:")
+a=input("Enter the character which you want to replace by space:")
+str1=str.replace(" ",a)
+print(str1)
